fdadabf81f8027d87116698a0ec6471046fcaf6c
sl4ppy <chris@thevandorens.com>
2023-06-20 19:53:20 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+pip3 install adafruit-circuitpython-ssd1306
+sudo apt-get install python3-pip
+sudo apt-get install python3-pil
+
+sudo pip3 install --upgrade setuptools
+
+cd ~
+sudo pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo python3 raspi-blinka.py
