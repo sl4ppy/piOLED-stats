d17b6dd1152c17bac0253e9629bcaa8f83d272ab
Chris Van Doren <chris@thevandorens.com>
2023-08-31 09:56:06 -0500

    add exit code

--- setup.sh
+++ setup.sh
@@ -10,3 +10,4 @@ cd ~
 sudo pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 sudo python3 raspi-blinka.py
+exit 0
